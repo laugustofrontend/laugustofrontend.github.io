32151d31e4d7b4e664f72bac2cf9b6d37f04e254
Lucas Augusto <lucas.augusto5061@gmail.com>
2017-03-22 19:34:21 -0300

    retirado excesso de cores na barra de habilidades, colocado títulos do site em português e feito ajustes de reponsividade

--- assets/javascripts/template.js
+++ assets/javascripts/template.js
@@ -2,7 +2,7 @@ function templateSkill (name, skill, colum) {
   return `
     <article class="skill col-xs-12 col-sm-6 row">
       <div class="skill_name col-xs-12">${name}</div>
-      <div class="skill_percent skill_${skill} col-xs-${colum}"></div>
+      <div class="skill_percent col-xs-${colum}"></div>
     </article>
   `
 }
